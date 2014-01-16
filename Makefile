cba69ebe68a11e308ab08dc15e8bb5e8e8d1d95f
Jason Dsouza <jasonrdsouza@gmail.com>
2014-01-15 21:55:21 -0500

    Remove makefile and rename cmd to cli

--- Makefile
+++
@@ -1,8 +0,0 @@
-run:
-	go run ./cmd/mop.go
-
-build:
-	go build ./cmd/mop.go
-
-install:
-	go install github.com/michaeldv/mop/cmd
--- mop-cli/mop.go
+++ mop-cli/mop.go
@@ -5,7 +5,7 @@
 package main
 
 import (
-	`github.com/michaeldv/mop`
+	`github.com/jasonrdsouza/mop`
 	`github.com/michaeldv/termbox-go`
 	`time`
 )
